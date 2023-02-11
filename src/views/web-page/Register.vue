0f73b81329f1a6dc0baf67fb4c905b7325a2cbe5
Rafal Szczotka <rafalszczotka@wp.pl>
2023-02-11 03:06:00 +0000

    Changed style of progress bar

--- src/views/web-page/Register.vue
+++ src/views/web-page/Register.vue
@@ -2,23 +2,24 @@
   <nav-menu></nav-menu>
   <div>
     <div class="progress-bar">
-      <div
-        v-for="(step, index) in 3"
-        :key="index"
-        :class="{ active: index + 1 === currentStep }"
-      />
+      <div class="progress-bar-fill" :style="{ width: `${percent}%` }">
+        <h2>{{ currentFormName }}</h2>
+      </div>
     </div>
-    <div v-show="currentStep === 1">
-      <Studio-form @studioDto-data="handleStudioDtoInputs" />
-    </div>
-    <div v-show="currentStep === 2">
-      <Address-form
-        @addressDto-data="handleAddressDtoInputs"
-        @go-back="goBack"
-      />
-    </div>
-    <div v-show="currentStep === 3">
-      <User-form @submit="handleEmployeeDtoInputs" @go-back="goBack" />
+
+    <div class="forms">
+      <div v-show="currentStep === 1">
+        <Studio-form @studioDto-data="handleStudioDtoInputs" />
+      </div>
+      <div v-show="currentStep === 2">
+        <Address-form
+          @addressDto-data="handleAddressDtoInputs"
+          @go-back="goBack"
+        />
+      </div>
+      <div v-show="currentStep === 3">
+        <User-form @submit="handleEmployeeDtoInputs" @go-back="goBack" />
+      </div>
     </div>
   </div>
   <the-footer></the-footer>
@@ -27,7 +28,7 @@
 <script>
 import { ref, reactive } from "vue";
 import { useStore } from "vuex";
-import { useRouter } from "vue-router"
+import { useRouter } from "vue-router";
 import NavMenu from "@/components/web-page/nav/NavMenu.vue";
 import TheFooter from "@/components/web-page/footer/TheFooter.vue";
 //import axios from "axios";
@@ -48,6 +49,8 @@ export default {
     const studioStore = useStore();
     const router = useRouter();
     const currentStep = ref(1);
+    let percent = ref(33);
+    let currentFormName = ref('Studio\'s information');
 
     const nextStep = () => {
       currentStep.value++;
@@ -80,6 +83,10 @@ export default {
       employeeDto: {},
     });
 
+    function updateProgress(value) {
+      percent.value = value;
+    }
+
     function handleStudioDtoInputs(data) {
       createStudioRequest.studioDto.name = data.name;
       createStudioRequest.studioDto.nip = data.nip;
@@ -88,6 +95,8 @@ export default {
       createStudioRequest.studioDto.email = data.email;
 
       nextStep();
+      updateProgress(66);
+      currentFormName.value = 'Address\'s information';
     }
 
     function handleAddressDtoInputs(data) {
@@ -99,12 +108,17 @@ export default {
       createStudioRequest.studioDto.addressDto.houseNumber = data.houseNumber;
 
       nextStep();
+      updateProgress(100);
+      currentFormName.value = 'User\'s information';
     }
 
     async function handleEmployeeDtoInputs(data) {
       createStudioRequest.employeeDto = data;
       //studioStore.dispatch('studio/addStudio', createStudioRequest);
-      const response = await studioStore.dispatch('studio/addStudio', createStudioRequest);
+      const response = await studioStore.dispatch(
+        "studio/addStudio",
+        createStudioRequest
+      );
       if (response.status === 200) {
         //redirect to login page
         router.push("/login");
@@ -114,23 +128,16 @@ export default {
       }
     }
 
-    // async function sendRequest(data) {
-      // try {
-      //   const response = await axios.post("http://localhost:8080/studio/create", data);
-      //   //console.log(response.status);
-      //   return response.data;
-      // } catch (error) {
-      //   console.error(error.response.data);
-      // }
-    // }
     return {
+      percent,
       currentStep,
+      currentFormName,
       nextStep,
       previousStep,
       handleStudioDtoInputs,
       handleAddressDtoInputs,
       handleEmployeeDtoInputs,
-      goBack
+      goBack,
     };
   },
 };
@@ -139,18 +146,27 @@ export default {
 
 <style scoped>
 .progress-bar {
+  width: 100%;
+  height: 3rem;
+  background-color: #ddd;
+  border-radius: 5px;
+  position: relative;
+}
+
+.progress-bar-fill {
+  height: 100%;
+  background-color: #FC4A1A;
+  border-radius: 5px;
+  transition: width 0.25s ease-in-out;
   display: flex;
+  justify-content: center;
 }
 
-.progress-bar div {
-  width: 33.33%;
-  height: 10px;
-  background: lightgray;
-  border-radius: 10px;
-  margin-right: 10px;
+h2 {
+  color: #FFFEFE;
 }
 
-.progress-bar div.active {
-  background: blue;
+.forms {
+  margin-top: 2rem;
 }
 </style>
