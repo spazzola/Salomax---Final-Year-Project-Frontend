7ae3dab9cab0e834807a0fb9648cc7473093d9fb
Rafal Szczotka <rafalszczotka@wp.pl>
2023-02-14 01:16:06 +0000

    Added FontAwesome to the project

--- src/main.js
+++ src/main.js
@@ -6,4 +6,13 @@ import store from './store'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
 
-createApp(App).use(store).use(router).mount('#app')
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas);
+import { far } from '@fortawesome/free-regular-svg-icons';
+library.add(far);
+import { dom } from "@fortawesome/fontawesome-svg-core";
+dom.watch();
+
+createApp(App).use(store).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
