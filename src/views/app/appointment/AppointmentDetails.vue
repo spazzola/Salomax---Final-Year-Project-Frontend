adc9019b75bd253b9261323bc0e515b2010df7b8
Rafal Szczotka <rafalszczotka@wp.pl>
2023-02-17 02:18:59 +0000

    Added smaller calendar for navigation. Added functionality to open AppointmentDetails as a modal window. Added skeleton for AppointmentDetails

---
+++ src/views/app/appointment/AppointmentDetails.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="loading-overlay" v-if="showDialog" @click="hideDialog">
+    <div class="my-modal" @click.stop></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppointmentDetails",
+  props: {
+    showDialog: {
+      type: Boolean,
+      default: false
+    }
+  },
+  setup(props, context) {
+    function hideDialog() {
+      context.emit('hide-dialog');
+    }
+
+    return { hideDialog }
+  },
+}
+</script>
+
+<style scoped>
+.loading-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(255, 255, 255, 0.5);
+  filter: blur(4px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 10;
+}
+
+.my-modal {
+  width: 30rem;
+  height: 20rem;
+  z-index: 100;
+  background-color: red;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+}
+</style>
--- src/views/app/appointment/Appointments.vue
+++ src/views/app/appointment/Appointments.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="container" style="height: 100%">
+  <appointment-details :showDialog="showDialog" @hide-dialog="hideDialog"/>
+
+  <div class="container" style="height: 100%; width: 100vw">
     <div class="row header">
       <div class="col-5">
         <header>
@@ -11,22 +13,41 @@
       <div class="col-xl-3 col-md-4 col-sm-4" style="height: 100%">
         <left-menu />
       </div>
-
+      <div class="col-xl-3">
+        <base-button text="New Appointment" class="btn-add-appointment" />
+        <vue-cal
+          class="vuecal--rounded-theme vuecal--green-theme"
+          xsmall
+          hide-view-selector
+          :time="false"
+          active-view="month"
+          :disable-views="['week']"
+          style="width: 100%; height: 300px"
+          @cell-click="changeSelectedDate('cell-click', $event)"
+        >
+        </vue-cal>
+      </div>
       <VueCal
         id="vuecal"
+        class="col-xl-9"
         style="height: 720px"
         :time-from="6 * 60"
         :time-to="22 * 60"
         :time-step="30"
         :time-cell-height="60"
+        :hideViewSelector="false"
         :disable-views="['years', 'year']"
         @ready="scrollToCurrentTime"
+        :on-event-click="onEventClick"
+        :events="events"
+        :selected-date="selectedDate"
         ref="vuecal"
       >
-        >
+        <template #event="{ event }">
+          <appointment-card :event="event" :showDialog="showDialog" />
+        </template>
       </VueCal>
     </div>
-
     <the-footer />
   </div>
 </template>
@@ -35,8 +56,11 @@
 import { ref } from "vue";
 import LeftMenu from "../../../components/app/leftmenu/LeftMenu.vue";
 import TheFooter from "../../../components/web-page/footer/TheFooter.vue";
+import AppointmentCard from "./AppointmentCard.vue";
+import BaseButton from "../../../components/common/buttons/BaseButton.vue";
 import VueCal from "vue-cal";
 import "vue-cal/dist/vuecal.css";
+import AppointmentDetails from './AppointmentDetails.vue';
 
 export default {
   name: "Appointments",
@@ -44,20 +68,104 @@ export default {
     LeftMenu,
     TheFooter,
     VueCal,
+    AppointmentCard,
+    BaseButton,
+    AppointmentDetails,
   },
   setup() {
     const currentDate = ref(new Date());
+    const selectedDate = ref(currentDate.value);
+    const events = [
+      {
+        start: "2023-02-13 16:00",
+        end: "2023-02-13 16:05",
+        title: "Alice",
+        price: 30,
+        duration: 5,
+      },
+      {
+        start: "2023-02-14 16:00",
+        end: "2023-02-14 16:10",
+        title: "Alice",
+        price: 30,
+        duration: 10,
+      },
+      {
+        start: "2023-02-15 16:00",
+        end: "2023-02-15 16:15",
+        title: "Alice",
+        price: 30,
+        duration: 15,
+      },
+      {
+        start: "2023-02-16 16:00",
+        end: "2023-02-16 16:20",
+        title: "Alice",
+        price: 30,
+        duration: 20,
+      },
+      {
+        start: "2023-02-17 16:00",
+        end: "2023-02-17 16:30",
+        title: "Joanna",
+        price: 50,
+        duration: 30,
+      },
+      {
+        start: "2023-02-18 16:00",
+        end: "2023-02-18 16:40",
+        title: "Alice",
+        price: 30,
+        duration: 40,
+      },
+      {
+        start: "2023-02-19 16:00",
+        end: "2023-02-19 16:50",
+        title: "Alice",
+        price: 30,
+        duration: 50,
+      },
+    ];
+
+    let selectedEvent = {};
+    const showDialog = ref(false);
 
     function scrollToCurrentTime() {
       const calendar = document.querySelector("#vuecal .vuecal__bg");
-      const hours = currentDate.value.getHours() + currentDate.value.getMinutes() / 60;
+      const hours =
+        currentDate.value.getHours() + currentDate.value.getMinutes() / 60;
       calendar.scrollTo({
         top: hours * 60,
         behavior: "smooth",
       });
     }
 
-    return { scrollToCurrentTime };
+    function onEventClick(event, e) {
+      selectedEvent = event;
+      showDialog.value = true;
+      // console.log("triggered");
+      // Prevent navigating to narrower view (default vue-cal behavior).
+      e.stopPropagation();
+    }
+
+    function changeSelectedDate(event, e) {
+      selectedDate.value = e;
+    }
+
+    function hideDialog() {
+      showDialog.value = false;
+    }
+
+    return {
+      selectedDate,
+      scrollToCurrentTime,
+      onEventClick,
+      events,
+      selectedEvent,
+      showDialog,
+      changeSelectedDate,
+      hideDialog,
+    };
   },
 };
 </script>
@@ -88,7 +196,7 @@ h1 {
   flex-wrap: nowrap;
 }
 
-.vuecal {
+.vuecal::before {
   padding: 0;
 }
 
@@ -98,5 +206,13 @@ h1 {
 
 .vuecal__title-bar {
   background-color: blue !important;
+}
+
+.btn-add-appointment {
+  /* color: black !important; */
+  width: 100%;
+  height: 45.7px;
+  border-bottom-left-radius: 0;
+  border-bottom-right-radius: 0;
 }
 </style>
