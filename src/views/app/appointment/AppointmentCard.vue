d0f340a915ac66de29f89e90014d9d744aa462fb
Rafal Szczotka <rafalszczotka@wp.pl>
2023-02-18 12:30:38 +0000

    Added AppointmentCard to display appointments on vue cal

---
+++ src/views/app/appointment/AppointmentCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="vuecal__event-title" v-html="event.clientDto.name" />
+    <p v-if="event.duration > 30">{{ event.service }}</p>
+    <p v-if="event.duration >= 30">Total: £{{ event.price }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppointmentCard",
+  // props: ["event", "showDialogg"],
+  props: {
+    event: {
+      type: Object,
+      default: null,
+    }
+  },
+  setup() {
+
+
+    return {  };
+  },
+};
+</script>
