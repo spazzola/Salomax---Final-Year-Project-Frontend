9d59404e56e6c0991b086bab083652a9b9ae8587
Rafal Szczotka <rafalszczotka@wp.pl>
2023-02-18 12:29:34 +0000

    Created template for displaying appointment's details

--- src/views/app/appointment/AppointmentDetails.vue
+++ src/views/app/appointment/AppointmentDetails.vue
@@ -1,53 +1,93 @@
 <template>
-  <div class="loading-overlay" v-if="showDialog" @click="hideDialog">
-    <div class="my-modal" @click.stop></div>
+  <div v-if="showDialog" @click="hideDialog">
+    <!-- <div class="my-modal" @click.stop></div> -->
+    <base-modal>
+      <template #header class="header">
+        <h2>{{ appointment.clientDto.name }} - {{ appointment.date }}</h2>
+      </template>
+      <template #content class="content">
+        <div style="display: flex; flex-direction: row; justify-content: space-around;">
+          <div class="left-content">
+            <strong><p>Client details:</p></strong>
+            <p>
+              {{ appointment.clientDto.name }}
+              {{ appointment.clientDto.surname }}
+            </p>
+            <p>{{ appointment.clientDto.phoneNumber }}</p>
+
+            <strong style="margin-top: 0.7rem"><p>Services:</p></strong>
+            <p v-for="work in appointment.appointmentDetailsDto" :key="work.id">
+              {{ work.name }}
+            </p>
+          </div>
+          <div class="right-content">
+            <strong><p>Note:</p></strong>
+          </div>
+        </div>
+      </template>
+    </base-modal>
   </div>
 </template>
 
 <script>
+import BaseModal from "../../../components/common/dialogs/BaseModal.vue";
+
 export default {
+  components: { BaseModal },
   name: "AppointmentDetails",
   props: {
     showDialog: {
       type: Boolean,
-      default: false
-    }
+      default: false,
+    },
+    appointment: {
+      type: Object,
+      default: null,
+    },
   },
   setup(props, context) {
     function hideDialog() {
-      context.emit('hide-dialog');
+      context.emit("hide-dialog");
     }
 
-    return { hideDialog }
+    return { hideDialog };
   },
-}
+};
 </script>
 
 <style scoped>
-.loading-overlay {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: rgba(255, 255, 255, 0.5);
-  filter: blur(4px);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  z-index: 10;
+h2 {
+  margin-bottom: 0 !important;
 }
 
-.my-modal {
-  width: 30rem;
-  height: 20rem;
-  z-index: 100;
-  background-color: red;
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  margin: auto;
+.header {
+  display: flex !important;
 }
+
+strong {
+  text-align: left;
+  margin-bottom: 0.3rem;
+}
+
+.left-content {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  align-content: flex-start;
+}
+
+.righ-content {
+  width: 100%;
+}
+
+p {
+  margin: 0;
+}
+
+/* section {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  align-content: flex-start;
+} */
 </style>
