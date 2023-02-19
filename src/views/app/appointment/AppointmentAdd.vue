76ba7176c3e85ee9efd2f5f2cdb1a7ba484b2eb3
Rafal Szczotka <rafalszczotka@wp.pl>
2023-02-19 23:55:27 +0000

    Created AppointmentAdd component

---
+++ src/views/app/appointment/AppointmentAdd.vue
@@ -0,0 +1,67 @@
+<template>
+  <base-grow-spinner :loading="loading" />
+
+  <div v-if="showDialog" @click="hideDialog">
+    <base-modal>
+      <template #header class="header">
+        <h2>New appointment</h2>
+      </template>
+      <template #content class="content">
+        <appointment-form @hide-dialog="hideDialog" @submit-form="submitForm" />
+      </template>
+    </base-modal>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import { useStore } from "vuex";
+import BaseModal from "../../../components/common/dialogs/BaseModal.vue";
+import AppointmentForm from "./AppointmentForm.vue";
+import BaseGrowSpinner from "../../../components/common/loading/BaseGrowSpinner.vue";
+
+export default {
+  name: "AppointmentAdd",
+  props: {
+    showDialog: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  components: {
+    BaseModal,
+    AppointmentForm,
+    BaseGrowSpinner
+  },
+  setup(props, context) {
+    const loading = ref(false);
+
+    const store = useStore();
+    function hideDialog() {
+      context.emit("hide-dialog");
+    }
+
+    async function submitForm(createAppointmentRequest) {
+      loading.value = true;
+      let response = await store.dispatch(
+        "appointment/addAppointment",
+        createAppointmentRequest
+      );
+      loading.value = false;
+      if (response.status === 200) {
+        //go to appointments and update appointments list
+      } else {
+        console.log(response);
+      }
+    }
+
+    return { hideDialog, submitForm };
+  },
+};
+</script>
+
+<style scoped>
+.content {
+  height: 50rem;
+}
+</style>
