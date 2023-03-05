d7b8daa8babce7554e0b232e377906a80c57a485
Rafal Szczotka <rafalszczotka@wp.pl>
2023-03-05 21:18:07 +0000

    Created new view for displaying Client's details

---
+++ src/views/app/clients/ClientDetails.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="container" style="height: 100%">
+    <div class="row header">
+      <div class="col-5">
+        <header>
+          <h1>S-max</h1>
+        </header>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xl-3 col-md-4 col-sm-4" style="height: 100%">
+        <left-menu />
+      </div>
+      <div class="col-xl-9 col-md-8, col-sm-8" style="border: 2px solid black;">
+        <h1>Client details</h1>
+      </div>
+    </div>
+
+    <the-footer />
+  </div>
+</template>
+
+<script>
+import LeftMenu from "../../../components/app/leftmenu/LeftMenu.vue";
+import TheFooter from "../../../components/web-page/footer/TheFooter.vue";
+
+export default {
+  name: "ClientDetails",
+  components: {
+    LeftMenu,
+    TheFooter
+  },
+  setup() {
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+}
+
+.header {
+  height: 15% !important;
+  text-align: left;
+  padding: 1rem;
+}
+
+.row {
+  height: 100%;
+}
+</style>
