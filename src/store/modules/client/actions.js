0de27bfabc550a58773bbab438e779cdf4fb30e0
Rafal Szczotka <rafalszczotka@wp.pl>
2023-03-08 19:14:33 +0000

    Added actions to module Client: add and delete

--- src/store/modules/client/actions.js
+++ src/store/modules/client/actions.js
@@ -12,4 +12,23 @@ export default {
       let clients = response.data
       context.commit('setClients', clients);
     },
+
+    async addClient(context, data) {
+      try {
+        const response = await axios.post(baseURL + "/client/create", data);
+        return response;
+      } catch (error) {
+        return error.response.data;
+      }
+    },
+
+    async deleteClient(context, params) {
+      try {
+        const response = await axios.delete(baseURL + "/client/deleteAssignedClient", { params });
+        return response
+      } catch (error) {
+        return error.response.dsta;
+      }
+    }
+
 }
