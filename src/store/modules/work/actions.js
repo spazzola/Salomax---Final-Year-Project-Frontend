3347180fdddb9666251052375eaf7b29c9fb9480
Rafal Szczotka <rafalszczotka@wp.pl>
2023-03-12 23:57:51 +0000

    Added new action to work's module. Refactor

--- src/store/modules/work/actions.js
+++ src/store/modules/work/actions.js
@@ -2,14 +2,19 @@ import { baseURL } from '../../../base-url.js';
 import axios from "axios";
 
 export default {
-    async loadWorks(context, id) {
-      const response = await axios.get(baseURL + "/work/getWorks", {
-        params: {
-          studioId: id
-        }
-      });
+  async loadWorks(context, id) {
+    const response = await axios.get(baseURL + "/work/getWorks", {
+      params: {
+        studioId: id
+      }
+    });
 
-      let works = response.data
-      context.commit('setWorks', works);
-    },
+    let works = response.data
+    context.commit('setWorks', works);
+  },
+
+  async setWork(context, data) {
+    context.commit('setWork', data);
+  }
+  
 }
