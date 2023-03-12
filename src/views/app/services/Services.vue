68ebca600cea38aa87730a6c0c22d8e0d316a62c
Rafal Szczotka <rafalszczotka@wp.pl>
2023-03-12 23:42:50 +0000

    Added BaseListView to Services view

--- src/views/app/services/Services.vue
+++ src/views/app/services/Services.vue
@@ -11,6 +11,18 @@
       <div class="col-xl-3 col-md-4 col-sm-4" style="height: 100%">
         <left-menu />
       </div>
+      <div class="col-xl-9 col-md-8, col-sm-8" style="border: 2px solid black">
+        <div class="row">
+          <div class="col-xl-2">
+            <!-- <base-button text="+ New service" @clicked="openClientAddDialog" /> -->
+          </div>
+          <base-list-view
+            class="col-xl-10"
+            :items="services"
+            @item-clicked="navigateToServiceDetails"
+          />
+        </div>
+      </div>
     </div>
 
     <the-footer />
@@ -18,18 +30,42 @@
 </template>
 
 <script>
-import LeftMenu from '../../../components/app/leftmenu/LeftMenu.vue';
-import TheFooter from '../../../components/web-page/footer/TheFooter.vue';
+import { ref, onBeforeMount } from "vue";
+import { useStore } from "vuex";
+import LeftMenu from "../../../components/app/leftmenu/LeftMenu.vue";
+import TheFooter from "../../../components/web-page/footer/TheFooter.vue";
+import BaseListView from "../../../components/app/list-view/BaseListView.vue";
 
 export default {
   components: {
     LeftMenu,
-    TheFooter
+    TheFooter,
+    BaseListView
   },
   setup() {
-    
+    const store = useStore();
+    const services = ref([]);
+
+    async function fetchData() {
+      const studioId = localStorage.getItem("studioId");
+
+      await store.dispatch("work/loadWorks", studioId);
+      const fetchedServices = store.getters["work/getAllWorks"];
+      services.value = fetchedServices.map((service) => {
+        return {
+          title: service.name + " - " + service.minutesDuration + "min, £" + service.price,
+          id: service.id,
+        };
+      });
+    }
+
+    onBeforeMount(async () => {
+      await fetchData();
+    });
+
+    return { services };
   },
-}
+};
 </script>
 
 <style scoped>
