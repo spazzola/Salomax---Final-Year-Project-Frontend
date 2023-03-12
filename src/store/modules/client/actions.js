f1872d136a143af902110d26b36533f13ccfe313
Rafal Szczotka <rafalszczotka@wp.pl>
2023-03-12 23:51:41 +0000

    Refactor

--- src/store/modules/client/actions.js
+++ src/store/modules/client/actions.js
@@ -2,45 +2,45 @@ import { baseURL } from '../../../base-url.js';
 import axios from "axios";
 
 export default {
-    async loadClients(context, studioId) {
-      const response = await axios.get(baseURL + "/client/getClients", {
-        params: {
-          studioId
-        }
-      });
-
-      let clients = response.data
-      context.commit('setClients', clients);
-    },
-
-    async addClient(context, data) {
-      try {
-        const response = await axios.post(baseURL + "/client/create", data);
-        return response;
-      } catch (error) {
-        return error.response.data;
+  async loadClients(context, studioId) {
+    const response = await axios.get(baseURL + "/client/getClients", {
+      params: {
+        studioId
       }
-    },
+    });
 
-    async editClient(context, data) {
-      try {
-        const response = await axios.put(baseURL + "/client/update", data);
-        return response;
-      } catch (error) {
-        return error.response.data;
-      }
-    },
+    let clients = response.data
+    context.commit('setClients', clients);
+  },
 
-    async deleteClient(context, params) {
-      try {
-        const response = await axios.delete(baseURL + "/client/deleteAssignedClient", { params });
-        return response
-      } catch (error) {
-        return error.response.dsta;
-      }
-    },
+  async addClient(context, data) {
+    try {
+      const response = await axios.post(baseURL + "/client/create", data);
+      return response;
+    } catch (error) {
+      return error.response.data;
+    }
+  },
 
-    async setClient(context, data) {
-      context.commit('setClient', data);
+  async editClient(context, data) {
+    try {
+      const response = await axios.put(baseURL + "/client/update", data);
+      return response;
+    } catch (error) {
+      return error.response.data;
+    }
+  },
+
+  async deleteClient(context, params) {
+    try {
+      const response = await axios.delete(baseURL + "/client/deleteAssignedClient", { params });
+      return response
+    } catch (error) {
+      return error.response.dsta;
+    }
+  },
+
+  async setClient(context, data) {
+    context.commit('setClient', data);
   }
 }
