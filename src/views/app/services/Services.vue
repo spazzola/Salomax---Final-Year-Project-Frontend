11e81a40edd40869c48cd23ba0e11791a39de252
Rafal Szczotka <rafalszczotka@wp.pl>
2023-03-13 16:25:04 +0000

    Added ServiceAdd to Services component

--- src/views/app/services/Services.vue
+++ src/views/app/services/Services.vue
@@ -1,4 +1,5 @@
 <template>
+  <service-add :showDialog="showServiceAddDialog" @hide-dialog="hideDialog" />
   <div class="container" style="height: 100%">
     <div class="row header">
       <div class="col-5">
@@ -14,7 +15,7 @@
       <div class="col-xl-9 col-md-8, col-sm-8" style="border: 2px solid black">
         <div class="row">
           <div class="col-xl-2">
-            <!-- <base-button text="+ New service" @clicked="openClientAddDialog" /> -->
+            <base-button text="+ New service" @clicked="openServiceAddDialog" />
           </div>
           <base-list-view
             class="col-xl-10"
@@ -36,17 +37,22 @@ import { useRouter } from "vue-router";
 import LeftMenu from "../../../components/app/leftmenu/LeftMenu.vue";
 import TheFooter from "../../../components/web-page/footer/TheFooter.vue";
 import BaseListView from "../../../components/app/list-view/BaseListView.vue";
+import BaseButton from "../../../components/common/buttons/BaseButton.vue";
+import ServiceAdd from "./ServiceAdd.vue";
 
 export default {
   components: {
     LeftMenu,
     TheFooter,
-    BaseListView
+    BaseListView,
+    BaseButton,
+    ServiceAdd
   },
   setup() {
     const store = useStore();
     const router = useRouter();
     const services = ref([]);
+    const showServiceAddDialog = ref(false);
 
     async function fetchData() {
       const studioId = localStorage.getItem("studioId");
@@ -55,23 +61,45 @@ export default {
       const fetchedServices = store.getters["work/getAllWorks"];
       services.value = fetchedServices.map((service) => {
         return {
-          title: service.name + " - " + service.minutesDuration + "min, £" + service.price,
+          title:
+            service.name +
+            " - " +
+            service.minutesDuration +
+            "min, £" +
+            service.price,
           id: service.id,
         };
       });
     }
 
     function navigateToServiceDetails(event) {
-      const selectedService = store.getters["work/getAllWorks"].find(w => w.id === event.id);
+      const selectedService = store.getters["work/getAllWorks"].find(
+        (w) => w.id === event.id
+      );
       store.dispatch("work/setWork", selectedService);
       router.push("/services/details/" + selectedService.id);
+    }
+
+    function openServiceAddDialog() {
+      showServiceAddDialog.value = true;
+    }
+
+    function hideDialog() {
+      showServiceAddDialog.value = false;
+      fetchData();
     }
 
     onBeforeMount(async () => {
       await fetchData();
     });
 
-    return { services, navigateToServiceDetails };
+    return {
+      services,
+      navigateToServiceDetails,
+      openServiceAddDialog,
+      showServiceAddDialog,
+      hideDialog
+    };
   },
 };
 </script>
