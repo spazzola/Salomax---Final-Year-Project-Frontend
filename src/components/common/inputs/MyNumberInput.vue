35a4eb648cd1e50cfa05adc221f8ccfeb5125991
Rafal Szczotka <rafalszczotka@wp.pl>
2023-03-13 17:21:57 +0000

    Created new component MyNumberInput

---
+++ src/components/common/inputs/MyNumberInput.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <input :value="inputValue" :placeholder="placeholder" :isValid="isValid" @input="emitEvent" class="form-control" :class="{invalid: !isValid}" type="number"/>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  name: 'MyNumberInput',
+  props: {
+    placeholder: {
+      type: String,
+      default: 'text'
+    },
+    isValid: {
+      type: Boolean,
+      default: true
+    },
+    // type: {
+    //   type: String,
+    //   default: ''
+    // },
+    defaultValue: {
+      type: Number
+    }
+  },
+  setup(props, context) {
+    const inputValue = ref(props.defaultValue);
+
+
+    function updateValue(event) {
+      inputValue.value = event.target.value;
+    }
+
+    function emitEvent(event) {
+      inputValue.value = event.target.value;
+      context.emit('text-input-finished', inputValue.value);
+    }
+
+    return {
+      inputValue,
+      updateValue,
+      emitEvent
+    };
+  }
+};
+</script>
+
+<style scoped>
+.form-control {
+  border: 2px solid #4abdac;
+  border-radius: 5px;
+}
+
+.form-control:active,
+.form-control:focus {
+  border-color: transparent;
+  border: 2px solid #4abdac;
+  box-shadow: 0 0 10px #4abdac;
+}
+
+.invalid {
+   border: 2px solid #FC4A1A !important;
+  animation: glow 1s ease-in-out infinite alternate;
+}
+
+@keyframes glow {
+  from {
+    box-shadow: 0 0 10px #4abdac;
+  }
+  to {
+    box-shadow: 0 0 20px #FC4A1A;
+  }
+}
+</style>
